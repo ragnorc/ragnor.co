8903194674efc1b3631e76d63b15c95211776420
Ragnor Comerford <hello@ragnor.co>
2020-05-30 14:38:50 +0400

    Add thoughts page.

---
+++ pages/thoughts.js
@@ -0,0 +1,14 @@
+import Layout from "components/layout";
+
+export default () => {
+  return (
+    <Layout>
+      <div class="pl-18 lg:pl-32 md:w-3/4 lg:w-3/4 leading-relaxed">
+        <h1 class="text-5xl font-bold mb-4">
+          <span class="text-primary">T</span>houghts
+        </h1>
+        <p>Coming soon ...</p>
+      </div>
+    </Layout>
+  );
+};
